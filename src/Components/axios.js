dc22026e2743b623a99c8ec28c7008dbc0f1a509
Juan Tellez <jmtp12poke@gmail.com>
2022-01-07 19:17:32 -0600

    Deploy Front with herku Backend

--- src/Components/axios.js
+++ src/Components/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create( {
-    baseURL: "http://localhost:5000" 
+    baseURL: "https://serene-reaches-43898.herokuapp.com" 
 })
 
 export default instance
