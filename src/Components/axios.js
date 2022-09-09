3e7933aeabf43f6606f9f3debf8ada171f0921e7
Juan Tellez <jmtp12poke@gmail.com>
2022-09-08 21:42:50 -0500

    url backend changed

--- src/Components/axios.js
+++ src/Components/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create( {
-    baseURL: "https://serene-reaches-43898.herokuapp.com" 
+    baseURL: "https://fast-journey-80413.herokuapp.com/" 
 })
 
 export default instance
